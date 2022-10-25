658c696d2d083184bcf00e06687cf2c00dd24332
petrut586 <petrut586@morrisville.edu>
2022-10-25 11:44:21 -0400

    auth.py with correct info

--- 1020GolfCourse/website/auth.py
+++ 1020GolfCourse/website/auth.py
@@ -11,9 +11,7 @@ def survey():
     if request.method == 'POST':
         email = request.form.get('email')
         first_Name = request.form.get('first_Name')
-        # date = request.form.get('date')
         new_user = User(email=email, first_Name=first_Name)
-        # review_Date = Review(date = date)
         db.session.add(new_user)
         db.session.commit()
         
@@ -24,13 +22,21 @@ def survey():
 @auth.route('/question', methods=['GET','POST'])
 def question():
     if request.method == 'POST':
-        id = request.form['id']
+        # user_id = User.query.get()
+        
+        date = request.form.get('date')
         gender = request.form.get('gender')
         golf_Course = request.form.get('selection')
+        golf_Rating = request.form.get('golf_rating')
+        visit = request.form.get('visit')
+        golf_ball = request.form.get('Gball')
+        club = request.form.get('club')
+        review_Rate = request.form.get('review_rating')
+        add_Feedback = request.form.get('comment')
         
-        new_review = Review(gender=gender, golf_Course=golf_Course)
+        new_Review = Review(add_Feedback=add_Feedback, date=date, gender=gender, golf_Course=golf_Course, rating=golf_Rating, visits=visit, type_Golfball=golf_ball, club_Brand=club, review_Rate=review_Rate)
         
-        db.session.add(new_review)
+        db.session.add(new_Review)
         db.session.commit()
         
         flash('Review was Successfully Submited')
